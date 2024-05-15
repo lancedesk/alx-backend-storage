6efc2f7dbc260ab546e84497a50623ef32301cf8
Lance Desk <lncedesk@gmail.com>
2024-05-15 08:16:21 -0700

    Implement a get_page function (prototype: def get_page(url: str) -> str:). The core of the function is very simple. It uses the requests module to obtain the HTML content of a particular URL and returns it.

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,41 +8,38 @@ and to cache the HTML content of the URL for a specified period of time.
 
 import redis
 import requests
+from typing import Callable
 from functools import wraps
 
+redis_client = redis.Redis()
 
-def count_requests(expiration: int):
+
+def count_requests(method: Callable) -> Callable:
     """
     Decorator to count number of times a URL is requested and cache the result.
     """
 
-    def decorator(method):
+    @wraps(method)
+    def wrapper(url: str) -> str:
         """
         Wrapper function to count requests and cache the HTML content.
         """
 
-        @wraps(method)
-        def wrapper(url: str):
-            """
-            Wrapper function to count requests and cache the HTML content.
-            """
+        redis_client.incr(f"count:{url}")
+        cached_html = redis_client.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
 
-            redis_client = redis.Redis()
-            cache_key = f"page:{url}"
-            count_key = f"count:{url}"
-            redis_client.incr(count_key)
-            cached_html = redis_client.get(cache_key)
-            if cached_html:
-                return cached_html
-            result = method(url)
-            redis_client.setex(cache_key, expiration, result)
-            return result
+        html_content = method(url)
+        redis_client.set(f'count:{url}', 0)
+        redis_client.setex(f"cached:{url}", 10, html_content)
 
-        return wrapper
-    return decorator
+        return html_content
+
+    return wrapper
 
 
-@count_requests(expiration=10)
+@count_requests
 def get_page(url: str) -> str:
     """
     Fetch the HTML content of a URL.
@@ -53,6 +50,5 @@ def get_page(url: str) -> str:
     Returns:
         str: The HTML content of the URL.
     """
-
     response = requests.get(url)
     return response.text
