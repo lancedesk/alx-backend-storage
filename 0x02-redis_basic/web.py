8bde04d56e04b8e1acf49a2c2df4231febadbf55
Lance Desk <lncedesk@gmail.com>
2024-05-15 07:21:25 -0700

    Implement a get_page function (prototype: def get_page(url: str) -> str:). The core of the function is very simple. It uses the requests module to obtain the HTML content of a particular URL and returns it.

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -22,21 +22,19 @@ def count_requests(method: Callable) -> Callable:
     """
     Decorator to count number of times a URL is requested and cache the result.
     """
-
     @wraps(method)
     def wrapper(url) -> str:
         """
         Wrapper function to count requests and cache the HTML content.
         """
-
         redis_client.incr(f'count:{url}')
-        cached_html = redis_client.get(f'result:{url}')
-        if cached_html:
-            return cached_html.decode('utf-8')
-        cached_html = method(url)
+        result = redis_client.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
         redis_client.set(f'count:{url}', 0)
         redis_client.setex(f'result:{url}', 10, result)
-        return cached_html
+        return result
     return wrapper
 
 
@@ -51,5 +49,4 @@ def get_page(url: str) -> str:
     Returns:
         str: The HTML content of the URL.
     """
-    response = requests.get(url)
-    return response.text
+    return requests.get(url).text
